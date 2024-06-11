c78cf5a5a254e771bfe3af43ea2418cecb4fc9ef
richmond-andoh <richmondandoh00@gmail.com>
2024-06-11 14:20:28 +0000

    Find and render only one item on screen

--- src/app/[name].js
+++ src/app/[name].js
@@ -1,15 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { useLocalSearchParams } from 'expo-router';
+import { StyleSheet, Text, View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import exercises from "../../workout-data/data/exercises.json";
 
 const ExerciseDetailsScreen = () => {
   const params = useLocalSearchParams();
+
+  const exercise = exercises.find((item) => item.name === params.name);
+
+  if (!exercise) {
+    return <Text>No Exercise Found</Text>;
+  }
+
   return (
     <View>
-      <Text>Exercise Details: { params.name } </Text>
+      <Text>Exercise Details: {params.name} </Text>
     </View>
-  )
-}
+  );
+};
 
 export default ExerciseDetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
