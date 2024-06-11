f4d21d4d8b8732a4e81545a7ef1c9e98b538310a
richmond-andoh <richmondandoh00@gmail.com>
2024-06-11 12:56:55 +0000

    :New file added

---
+++ src/app/exercise.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const ExerciseDetailsScreen = () => {
+  return (
+    <View>
+      <Text>Exercise Details </Text>
+    </View>
+  )
+}
+
+export default ExerciseDetailsScreen;
+
+const styles = StyleSheet.create({})
